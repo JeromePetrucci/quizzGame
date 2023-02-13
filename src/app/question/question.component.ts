41842bf51887194fd707f8fad420265e4dc81d4d
JeromePetrucci <jerome.petrucci.99@gmail.com>
2023-02-13 16:18:07 +0100

    commit pre capital

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Answer, Category, Question } from '../question';
@@ -11,52 +11,52 @@ import { QuestionService } from '../question.service'
 })
 export class QuestionComponent {
 
-  constructor(private questionService: QuestionService) {}
+  constructor(private questionService: QuestionService) { }
 
   //question
-  question! :  Observable<Question[]> ;
-  alreadyAsked : number = 0;
+  question!: Observable<Question[]>;
+  alreadyAsked: number = 0;
 
   //counters
-  questionCounter : number = 0;
-  counterGoodAnswer : number = 0;
+  questionCounter: number = 0;
+  counterGoodAnswer: number = 0;
   percentage: number = this.getPercentage();
-  actualRow:{counter:number,type:"win"|"lose"} = {counter : 0, type : "win"}
+  actualRow: { counter: number, type: "win" | "lose" } = { counter: 0, type: "win" }
 
   //answers' index
   randomI: number = this.getRandomInt();
-  index0 : boolean = this.getIndex(0);
-  index1 : boolean = this.getIndex(1);
-  index2 : boolean = this.getIndex(2);
-  index3 : boolean = this.getIndex(3);
-  
+  index0: boolean = this.getIndex(0);
+  index1: boolean = this.getIndex(1);
+  index2: boolean = this.getIndex(2);
+  index3: boolean = this.getIndex(3);
+
   //result
-  result : string = ""; //"loading ...";
-  answered : boolean = false;
-  isCorrect : boolean = false;
+  result: string = ""; //"loading ...";
+  answered: boolean = false;
+  isCorrect: boolean = false;
 
   //information of menu selection
-  categoryAll : boolean = this.questionService.allCategories || this.questionService.categories.length < 1;
-  categories : Category[] = this.questionService.categories;
+  categoryAll: boolean = this.questionService.allCategories || this.questionService.categories.length < 1;
+  categories: Category[] = this.questionService.categories;
   difficultySelected: string = this.questionService.difficulty;
 
-  onAnswer (correct: boolean, question: Question) {
+  onAnswer(correct: boolean, question: Question) {
     this.answered = true;
-    this.questionCounter +=1;
+    this.questionCounter += 1;
     this.isCorrect = correct
 
-    if (correct){
+    if (correct) {
       this.counterGoodAnswer += 1;
-      this.result = "Well Done !!! the answer was " +  question.correctAnswer;
-      if (this.actualRow.type === "win") {this.actualRow.counter += 1}
+      this.result = "Well Done !!! the answer was " + question.correctAnswer;
+      if (this.actualRow.type === "win") { this.actualRow.counter += 1 }
       else {
         this.actualRow.type = "win";
         this.actualRow.counter = 1;
       }
     }
     else {
-      this.result = "Wrong, the answer was " +   question.correctAnswer;
-      if (this.actualRow.type === "lose") {this.actualRow.counter += 1}
+      this.result = "Wrong, the answer was " + question.correctAnswer;
+      if (this.actualRow.type === "lose") { this.actualRow.counter += 1 }
       else {
         this.actualRow.type = "lose";
         this.actualRow.counter = 1;
@@ -68,13 +68,13 @@ export class QuestionComponent {
   }
 
   //relaod a new question
-  nextQuestion() : void {
+  nextQuestion(): void {
     this.answered = false;
     this.result = "loading ...";
 
     this.question = this.questionService.getQuestion();
     this.randomI = this.getRandomInt();
-    this.index0  = this.getIndex(0);
+    this.index0 = this.getIndex(0);
     this.index1 = this.getIndex(1);
     this.index2 = this.getIndex(2);
     this.index3 = this.getIndex(3);
@@ -82,33 +82,32 @@ export class QuestionComponent {
   }
 
   //functiun that return true if the parameter is the index of the good answer
-  getIndex (i: number): boolean {
-    if (i === this.randomI) {return true}
+  getIndex(i: number): boolean {
+    if (i === this.randomI) { return true }
     else return false
-  } 
+  }
 
   //return 0,1,2 or 3 the random place of the good answer
   getRandomInt(): number {
     let max = Math.floor(3);
     return Math.floor(Math.random() * (max + 1));
-}
-
-getPercentage() :number {
-  if (this.questionCounter === 0 ){return 0}
-  else {
-    return Math.round((this.counterGoodAnswer / this.questionCounter )*10000)/100;
   }
-}
 
-sendQuestion(question: Question, answer: boolean):void {
-  console.log ("sending");
-  this.questionService.addQuestion(question);
-  let newAnswer: Answer = {category: question.category, answer:answer, difficulty:question.difficulty, id:question.id, asked:1}
-  this.alreadyAsked = this.questionService.addAnswer(newAnswer)
-  
-}
+  getPercentage(): number {
+    if (this.questionCounter === 0) { return 0 }
+    else {
+      return Math.round((this.counterGoodAnswer / this.questionCounter) * 10000) / 100;
+    }
+  }
+
+  sendQuestion(question: Question, answer: boolean): void {
+    console.log("sending");
+    this.questionService.addQuestion(question);
+    let newAnswer: Answer = { category: question.category, answer: answer, difficulty: question.difficulty, id: question.id, asked: 1 }
+    this.alreadyAsked = this.questionService.addAnswer(newAnswer)
+  }
 
   ngOnInit(): void {
     this.question = this.questionService.getQuestion();
-}
+  }
 }
